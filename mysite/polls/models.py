432d9e2efccaa7a9259fa342acd06f16c8210936
Lokesh Rengaraju <>
2025-04-09 13:30:48 -0700

    Customizing the admin site

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,6 +1,9 @@
 import datetime
 from django.utils import timezone
 from django.db import models
+from django.contrib import admin 
+
+
 
 # Create your models here.
 
@@ -10,6 +13,14 @@ class Question(models.Model):
     
     def __str__(self):
         return self.question_text
+
+    @admin.display(
+            boolean = True,
+            ordering = 'pub_date',
+            description = 'Published recently?',
+
+    )
+
 
     def was_published_recently(self):
         now = timezone.now()
